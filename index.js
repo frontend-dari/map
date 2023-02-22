ca3715e2cf7fade75537b0ececf8cd95b7170df7
frontend-dari <110198620+frontend-dari@users.noreply.github.com>
2023-02-22 12:58:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,56 @@ function init() {
     zoom: 12
   });
 }
+
+export default class GeoReview {
+  constructor() {
+    this.map = new InteractiveMap('map', this.onClick.bind(this));
+    this.map.init().then(this.onInit.bind(this));
+  }
+
+  
+  /* InteractiveMap */
+
+export default class InteractiveMap {
+  constructor(mapId, onClick) {
+    this.mapId = mapId;
+    this.onClick = onClick;
+  }
+
+  async init() {
+    await this.injectYMapsScript();
+    await this.loadYMaps();
+    this.initMap();
+  }
+
+  injectYMapsScript() {
+    return new Promise((resolve) => {
+      const ymapsScript = document.createElement('script');
+      ymapsScript.src =
+        'https://api-maps.yandex.ru/2.1/?apikey=f1f9f4be-9555-43d6-8412-1406842933b5&lang=ru_RU';
+      document.body.appendChild(ymapsScript);
+      ymapsScript.addEventListener('load', resolve);
+    });
+  }
+
+  loadYMaps() {
+    return new Promise((resolve) => ymaps.ready(resolve));
+  }
+
+  initMap() {
+    this.clusterer = new ymaps.Clusterer({
+      groupByCoordinates: true,
+      clusterDisableClickZoom: true,
+      clusterOpenBalloonOnClick: false,
+    });
+    this.clusterer.events.add('click', (e) => {
+      const coords = e.get('target').geometry.getCoordinates();
+      this.onClick(coords);
+    });
+    this.map = new ymaps.Map(this.mapId, {
+      center: [55.76, 37.64],
+      zoom: 10,
+    });
+    this.map.events.add('click', (e) => this.onClick(e.get('coords')));
+    this.map.geoObjects.add(this.clusterer);
+  }
